4f5bdb4accaf3857944f61615cf18179e47bf5a9
yuriestefano <yuriestefano155@gmail.com>
2023-08-17 19:35:04 -0300

    ..

--- js/Main.js
+++ js/Main.js
@@ -1,4 +1,10 @@
 const form=document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens= JSON.parse(localStorage.getItem('itens')) || []
+
+itens.forEach((elemento => {
+    console.log(elemento.nome, elemento.quantidade)
+}))
 
 form.addEventListener("submit", (event) =>{
     event.preventDefault()
@@ -6,10 +12,18 @@ form.addEventListener("submit", (event) =>{
    const nome=event.target.elements['nome'];
     const quantidade=event.target.elements['quantidade'];
 
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value}
+
 
 
  //evento de interação,são os dois inputs de nome  e quantidade
-    criaElemento(nome.value, quantidade.value)
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     nome.value=""
     quantidade.value=""
@@ -18,7 +32,7 @@ form.addEventListener("submit", (event) =>{
 
 
 //essa função recebe os dados de item e quantidade e os retorna adicionando na lista ul e estilizados com a tag strng.
-function criaElemento(nome, quantidade){
+function criaElemento(item){
 
 
 const novoItem= document.createElement('li')
@@ -26,18 +40,16 @@ novoItem.classList.add('item')
 
 
 const numeroItem= document.createElement('strong')
-numeroItem.innerHTML = quantidade
-
+numeroItem.innerHTML = item.quantidade
 novoItem.appendChild(numeroItem)
-novoItem.innerHTML += nome
 
-const lista= document.getElementById("lista2")
+novoItem.innerHTML += item.nome
 
 lista.appendChild(novoItem)
 
-console.log(novoItem)
-
-localStorage.setItem("novoItem", "numeroItem")
 
 
 }
+
+
+
